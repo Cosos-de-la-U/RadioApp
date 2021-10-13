d8b3fa7083262bc58b1735f5e8d3771fa8e5941f
Raul <raul.h.penate@gmail.com>
2021-10-12 19:40:23 -0600

    :hammer: [ADD] Radio
    :u now radio works

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:ai_radio/model/radio.dart';
 import 'package:ai_radio/utils/ai_utils.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -14,6 +16,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late List<MyRadio> radios;
+  late MyRadio _selectedRadio;
+  late Color _selectedColor;
+  bool _isPlaying = false;
+
+  final AudioPlayer _audioPlayer = AudioPlayer();
 
   @override
   void initState() {
@@ -21,12 +28,28 @@ class _HomePageState extends State<HomePage> {
     // TODO: implement initState
     super.initState();
     fetchRadios();
+
+    //Inicando toda la bayna
+    _audioPlayer.onPlayerStateChanged.listen((event) {
+      if (event == PlayerState.PLAYING) {
+        _isPlaying = true;
+      } else {
+        _isPlaying = false;
+      }
+    });
   }
 
   fetchRadios() async {
     final radioJson = await rootBundle.loadString("assets/radio.json");
     radios = MyRadioList.fromJson(radioJson).radios;
     //print(radios);
+    setState(() {});
+  }
+
+  _playMusic(String url) {
+    _audioPlayer.play(url);
+    _selectedRadio = radios.firstWhere((element) => element.url == url);
+    print(_selectedRadio);
     setState(() {});
   }
 
@@ -54,74 +77,92 @@ class _HomePageState extends State<HomePage> {
             elevation: 0.0,
             centerTitle: true,
           ).h(100.0).p16(),
-          VxSwiper.builder(
-            itemCount: radios.length,
-            aspectRatio: 1.0,
-            itemBuilder: (context, index) {
-              final rad = radios[index];
+          radios != null
+              ? VxSwiper.builder(
+                  itemCount: radios.length,
+                  aspectRatio: 1.0,
+                  itemBuilder: (context, index) {
+                    final rad = radios[index];
 
-              return VxBox(
-                      child: ZStack(
-                [
-                  Positioned(
-                      top: 0.0,
-                      left: 0.0,
-                      child: VxBox(
-                              child: rad.category.text.uppercase.teal100
-                                  .make()
-                                  .px16())
-                          .height(40)
-                          .pink700
-                          .alignBottomCenter
-                          .withRounded(value: 5.0)
-                          .make()),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: VStack(
+                    return VxBox(
+                            child: ZStack(
                       [
-                        rad.name.text.xl3.pink200.bold.make(),
-                        5.heightBox,
-                        rad.tagline.text.sm.pink100.semiBold.make(),
-                      ],
-                      crossAlignment: CrossAxisAlignment.center,
-                    ),
-                  ),
-                  Align(
-                      alignment: Alignment.center,
-                      // ignore: prefer_const_constructors
-                      child: [
-                        const Icon(
-                          CupertinoIcons.play_circle,
-                          color: Vx.teal100,
+                        Positioned(
+                            top: 0.0,
+                            left: 0.0,
+                            child: VxBox(
+                                    child: rad.category.text.uppercase.teal100
+                                        .make()
+                                        .px16())
+                                .height(40)
+                                .pink700
+                                .alignBottomCenter
+                                .withRounded(value: 5.0)
+                                .make()),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: VStack(
+                            [
+                              rad.name.text.xl3.pink200.bold.make(),
+                              5.heightBox,
+                              rad.tagline.text.sm.pink100.semiBold.make(),
+                            ],
+                            crossAlignment: CrossAxisAlignment.center,
+                          ),
                         ),
-                        10.heightBox,
-                        "Doble tap para reproducir".text.teal100.make()
-                      ].vStack())
-                ],
-                clip: Clip.antiAlias,
-              ))
-                  .clip(Clip.antiAlias)
-                  .bgImage(
-                    DecorationImage(
-                        image: NetworkImage(rad.image),
-                        fit: BoxFit.cover,
-                        colorFilter: ColorFilter.mode(
-                            Colors.black.withOpacity(0.3), BlendMode.darken)),
-                  )
-                  .border(color: Vx.pink700, width: 1.0)
-                  .withRounded(value: 60.0)
-                  .make()
-                  .onDoubleTap(() {})
-                  .p16();
-            },
-          ).centered(),
+                        Align(
+                            alignment: Alignment.center,
+                            // ignore: prefer_const_constructors
+                            child: [
+                              const Icon(
+                                CupertinoIcons.play_circle,
+                                color: Vx.teal100,
+                              ),
+                              10.heightBox,
+                              "Doble tap para reproducir".text.teal100.make()
+                            ].vStack())
+                      ],
+                      clip: Clip.antiAlias,
+                    ))
+                        .clip(Clip.antiAlias)
+                        .bgImage(
+                          DecorationImage(
+                              image: NetworkImage(rad.image),
+                              fit: BoxFit.cover,
+                              colorFilter: ColorFilter.mode(
+                                  Colors.black.withOpacity(0.3),
+                                  BlendMode.darken)),
+                        )
+                        .border(color: Vx.pink700, width: 1.0)
+                        .withRounded(value: 60.0)
+                        .make()
+                        .onInkTap(() {
+                      _playMusic(rad.url);
+                    }).p16();
+                  },
+                ).centered()
+              : Center(child: CircularProgressIndicator(backgroundColor: Colors.white)),
           // ignore: prefer_const_constructors
           Align(
-            alignment:Alignment.bottomCenter,
+            alignment: Alignment.bottomCenter,
             // ignore: prefer_const_constructors
-            child: Icon(
-              CupertinoIcons.stop_circle, color: Colors.white, size: 50.0
-              ),
+            child: [
+              if (_isPlaying)
+                "Playing now - ${_selectedRadio.name} FM".text.makeCentered(),
+              Icon(
+                _isPlaying
+                    ? CupertinoIcons.stop_circle
+                    : CupertinoIcons.play_circle,
+                color: Colors.white,
+                size: 50.0,
+              ).onInkTap(() {
+                if (_isPlaying) {
+                  _audioPlayer.stop();
+                } else {
+                  _playMusic(_selectedRadio.url);
+                }
+              })
+            ].vStack(),
           ).pOnly(bottom: context.percentHeight * 12)
         ],
         fit: StackFit.expand,
